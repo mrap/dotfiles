953fec66435fea9dc0c2a1e1b01df783c2a0465d
Mike Rapadas <mike@mrap.me>
2015-02-04 17:25:49 -0800

    Adds pbcopyfile function

--- zsh/mrap-aliases.zsh
+++ zsh/mrap-aliases.zsh
@@ -1,3 +1,16 @@
 function mkcd(){
   mkdir $1 && cd $1
 }
+
+pbcopyfile () {
+  if [ $# -eq 0 ]; then
+    echo "pbcopyfile requires a filename"
+    return 1;
+  elif [ ! -f $1 ]; then
+    echo "Can't copy contents because file $1 does not exist!"
+    return 1;
+  else
+    cat $1 | pbcopy
+    echo "Copied contents of $1 to clipboard!"
+  fi
+}
