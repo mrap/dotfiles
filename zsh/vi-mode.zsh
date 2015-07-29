6e0e18085bea4a6cca9ae6c4a3c820300a4e7f19
Michael Rapadas <michael.rapadas@otoy.com>
2015-07-29 04:38:59 -0700

    Fixes C-P and C-N bindings

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -9,6 +9,8 @@ bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 bindkey '^r' history-incremental-search-backward
+bindkey '^p' history-substring-search-up
+bindkey '^n' history-substring-search-down
 
 function zle-line-init zle-keymap-select {
   zle reset-prompt
