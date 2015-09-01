d3b5f86d1ba1eff27ea8565b20e911427355337d
Michael Rapadas <michael.rapadas@otoy.com>
2015-09-01 11:33:27 -0700

    Searchable git log

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -28,3 +28,26 @@ export FZF_DEFAULT_OPTS='
   --color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81
   --color info:144,prompt:161,spinner:135,pointer:135,marker:118
 '
+
+# fcs - get git commit sha
+# example usage: git rebase -i `fcs`
+fcs() {
+  local commits commit q
+  commits=$(
+  git log --color=always --pretty=oneline --abbrev-commit --reverse \
+    --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@") &&
+  commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
+  echo -n $(echo "$commit" | sed "s/ .*//")
+}
+
+fzf-fcs-widget() {
+  LBUFFER="${LBUFFER}$(fcs)"
+  zle redisplay
+}
+
+zle     -N   fzf-fcs-widget
+bindkey '^G' fzf-fcs-widget
+
+source "$yadr/zsh/fshow/gistfile1.sh"
+zle     -N   fshow
+bindkey '^S' fshow
