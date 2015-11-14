ab116fa84537b7a3aee85dfc3ffebf7d8ba35cbd
Mike Rapadas <mike@mrap.me>
2015-11-13 23:15:13 -0800

    Portable cvim

--- zsh/dev-tools.zsh
+++ zsh/dev-tools.zsh
@@ -1,4 +1,4 @@
-alias cvim_server='python3 ~/repos/chromium-vim/cvim_server.py'
+alias cvim_server='python3 $yadr/chrome/chromium-vim/cvim_server.py'
 if [ `psas cvim_server | wc -l` -lt 2 ]; then
   cvim_server &
 fi
