6bab5a60ae84d2c15bd94ac7ff65dbc558b147bf
Mike Rapadas <mike@mrap.me>
2016-02-24 18:01:43 -0800

    clonerepo helper
    This is idiomatic to a dev environment that follows a structure like $GOPATH/src
    
    Example:
    `$ clonerepo https://github.com/mrap/dotfiles.git`
    
    Above command will clone repo to ~/github.com/mrap/dotfiles and cd into it

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -2,3 +2,16 @@
 __git_files () {
     _wanted files expl 'local files' _files
 }
+
+clonerepo () {
+  if [ "$#" -ne 1 ]; then
+    echo "Pass a git url to clone."
+    return 0
+  fi
+
+  local url=$1
+  local repo_path=$(echo $url | awk -F"/|\.git$" '{print $3"/"$4"/"$5}')
+  local repo_dir=$HOME/$repo_path
+
+  git clone $url $repo_dir && cd $repo_dir
+}
