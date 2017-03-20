68104556285dba80d2871a4d5f695fee75aafc4d
Mike Rapadas <mike@mrap.me>
2017-03-19 20:25:10 -0700

    Move personal zshrc logic and lazy init slow shit

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -6,3 +6,24 @@ if [ -d $HOME/.zsh.after/ ]; then
 fi
 
 alias git='hub'
+
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
+# Android
+export ANDROID_HOME=/usr/local/opt/android-sdk
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# Lazy load slow inits like nvm
+export SANDBOXRC=$HOME/.yadr/zsh/sandboxrc
+source $HOME/.yadr/zsh/sandboxd/sandboxd
+
+# Run powerline daemon
+if [ -f `which powerline-daemon` ]; then
+  powerline-daemon -q
+fi
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
